cfe9228bcf752b86b7279427180e318957afc73c
Mpho <mpho.maleka3@gmail.com>
2017-10-02 15:13:54 +0200

    add logo, second commit

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,6 @@ class Post(models.Model):
     title = models.CharField(max_length = 140)
     body = models.TextField()
     date = models.DateTimeField(blank=True, null=True)
-    urladd = models.CharField(max_length = 140)
     image = models.ImageField(blank=True, null=True)
 
     def __str__(self):
